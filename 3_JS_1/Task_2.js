8ec51053a8bfadd20ef8096d79284154ff0981f4
alexking34 <alex.lemaev@mail.ru>
2020-03-11 01:33:02 +0300

    Working on it

--- 3_JS_1/Task_2.js
+++ 3_JS_1/Task_2.js
@@ -32,23 +32,3 @@ document.write(localUrlInfo.fileExtension);
 document.write("<br/>");
 
 //Задание 1.6
-document.write("<br/>");
-document.writeln("Ссылка: "); 
-document.write("https://vk.com/doc20232844_537595184?hash=e27858d8f98146752a&dl=0c19c3a98b99294ce6");
-let hrefWithQueryParams = "https://vk.com/doc20232844_537595184?hash=e27858d8f98146752a&dl=0c19c3a98b99294ce6";
-document.write("<br/>");
-document.writeln("Параметры: ");
-console.log(getQueryParams(hrefWithQueryParams)); // вывели параметры ссылки
-document.write("<br/>");
-
-function getQueryParams(href) {
-    let queryParams = {};
-    href.substring(href.indexOf('?') + 1, href.length).split('&') // взяли все после ?, выкинули &, параметры разделили на подстроки
-    .forEach(param => {
-        const paramKeyValue = param.split('='); // еще раз разделили для каждого параметра
-        queryParams[paramKeyValue[0]] = paramKeyValue[1]; // запихнули эти подстрочки в параметры
-        document.write(paramKeyValue[0], " = ",paramKeyValue[1]);
-        document.write("<br/>");
-    });
-    return queryParams;
-}
